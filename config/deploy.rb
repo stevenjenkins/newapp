1c0fd0d1b350f0a0cf709392599607ca125cd6e4
Steven Jenkins <steven.jenkins@gmail.com>
2014-07-07 23:55:59 -0400

    initial commit

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+load 'deploy/assets'
+
+set :application, "synaptian.com"
+set :repository,  "git@github.com:stevenjenkins/newapp.git"
+
+set :scm, :git
+set :user, "synaptian.com"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+
+set :use_sudo, false
+
+# initialize rvm by using a login shell
+default_run_options[:shell] = '/bin/bash--login'
+
+role :web, "104.131.230.81"
+role :app, "104.131.230.81"
+role :db, "104.131.230.81"
+role :web, "104.131.230.81", :primary => true
+
+set :keep_releases, 5
+# if you want to clean up old releases on each deploy uncomment this:
+after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Newapp::Application.config.secret_key_base = '10da085188614afdb39912460e01bcacdea479cafbc2f399f45f8a10040f88af6c86becf563e62757153a4b9a1c0ba27996be0d3df7a5e3aaac534fb212ad61c'
